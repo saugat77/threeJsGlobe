29f46b612aca2782467d68d70e05765a092cb761
Saugat <saugatpandey4@gmail.com>
2024-11-17 16:31:46 +0545

    added atmosphere

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import gsap from 'gsap';
 import * as THREE from "three";
 import vertexShader from './shaders/vertex.glsl'
 import fragmentShader from "./shaders/fragment.glsl";
 import globe from "./assets/Img/Globe.jpg";
+
+import atmosphereVertexShader from "./shaders/atmosphereVertex.glsl";
+import atmosphereFragmentShader from "./shaders/atmosphereFragment.glsl";
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -33,31 +37,45 @@ const sphere = new THREE.Mesh(
   })
 );
 scene.add(sphere);
-console.log(sphere);
 
 //create Atmosphere
 const atmosphere = new THREE.Mesh(
   new THREE.SphereGeometry(5, 50, 50),
   new THREE.ShaderMaterial({
-    vertexShader,
-    fragmentShader,
-    
+    vertexShader: atmosphereVertexShader,
+    fragmentShader: atmosphereFragmentShader,
+    side: THREE.DoubleSide, // Ensure both sides of the sphere are rendered
+    transparent: true,
+    depthWrite: false,
   })
 );
-atmosphere.scale.set(1.1,1.1, 1.1);
+atmosphere.scale.set(1.1, 1.1, 1.1);
 scene.add(atmosphere);
 
+const group = new THREE.Group()
+group.add(sphere)
+scene.add(group);
 
 camera.position.z = 15;
 
+const mouse = {
+  x: undefined,
+  y: undefined,
+};
+
 function animate() {
-  sphere.rotation.x += 0.01;
+  sphere.rotation.x += 0.001;
   sphere.rotation.y += 0.001;
-  atmosphere.rotation.x += 0.001;
-  atmosphere.rotation.y += 0.001;
-  atmosphere.rotation.z += 0.001;
+  group.rotation.y = mouse.x * 0.5;
+
   renderer.render(scene, camera);
 }
+
+addEventListener('mousemove',()=>{
+  mouse.x = (event.clientX/innerWidth) * 2-1
+  mouse.y = -(event.clientX / innerWidth) * 2 + 1;
+  console.log(mouse);
+})
 function App() {
   
 }
--- src/shaders/atmosphereVertex.glsl
+++ src/shaders/atmosphereVertex.glsl
@@ -2,7 +2,7 @@
 varying vec3 vertexNormal; 
 
 void main(){
-    vertexNormal = normal;
+    vertexNormal = normalize(normalMatrix *normal);
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
     
