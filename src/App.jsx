a7c0d44ed62af4d5448ecbcd796b6470af1a60cd
Saugat <saugatpandey4@gmail.com>
2024-11-28 12:42:23 +0545

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -56,6 +56,29 @@ const group = new THREE.Group()
 group.add(sphere)
 scene.add(group);
 
+const starGeometry = new THREE.BufferGeometry();
+const starMaterial = new THREE.PointsMaterial({
+  color:0xffffff
+});
+
+const starVertices = []
+
+for (let i = 0; i<10000 ;i++){
+  const x = (Math.random() - 0.5)*2000
+  const y =(Math.random() - 0.5)*2000
+  const z =-Math.random()*2000
+  starVertices.push(x,y,z)
+}
+
+console.log(starVertices);
+starGeometry.setAttribute('position', new THREE.Float32BufferAttribute(
+  starVertices, 3
+))
+const stars = new THREE.Points(starGeometry, starMaterial);
+
+console.log('star',stars);
+scene.add(stars);
+
 camera.position.z = 15;
 
 const mouse = {
@@ -67,7 +90,11 @@ function animate() {
   sphere.rotation.x += 0.001;
   sphere.rotation.y += 0.001;
   group.rotation.y = mouse.x * 0.5;
-
+  gsap.to(group.rotation,{
+    x: -mouse.y* 0.3,
+    y:mouse.x * 0.5,
+    duration:2
+  })
   renderer.render(scene, camera);
 }
 
